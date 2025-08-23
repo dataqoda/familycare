7f821aa81943babb4532f626deda5661b2b732da
Data Qoda <dataqoda@gmail.com>
2025-08-23 20:10:29 +0000

    Restored to 'fdad8cfc358618901c0c7dde6f2b2d92c54d1891'
    Replit-Restored-To: fdad8cfc358618901c0c7dde6f2b2d92c54d1891

--- client/src/hooks/use-mobile.tsx
+++ client/src/hooks/use-mobile.tsx
@@ -4,47 +4,16 @@ const MOBILE_BREAKPOINT = 768
 
 export function useIsMobile() {
   const [isMobile, setIsMobile] = React.useState<boolean | undefined>(undefined)
-  const [isLandscape, setIsLandscape] = React.useState<boolean>(false)
 
   React.useEffect(() => {
     const mql = window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`)
-    const landscapeMql = window.matchMedia("(orientation: landscape)")
-    
     const onChange = () => {
-      const mobile = window.innerWidth < MOBILE_BREAKPOINT || 
-                   (window.innerHeight < 500 && window.innerWidth < 900)
-      setIsMobile(mobile)
-      setIsLandscape(window.innerHeight < window.innerWidth)
+      setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
     }
-    
     mql.addEventListener("change", onChange)
-    landscapeMql.addEventListener("change", onChange)
-    onChange()
-    
-    return () => {
-      mql.removeEventListener("change", onChange)
-      landscapeMql.removeEventListener("change", onChange)
-    }
+    setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
+    return () => mql.removeEventListener("change", onChange)
   }, [])
 
   return !!isMobile
-}
-
-export function useIsLandscape() {
-  const [isLandscape, setIsLandscape] = React.useState<boolean>(false)
-
-  React.useEffect(() => {
-    const landscapeMql = window.matchMedia("(orientation: landscape)")
-    
-    const onChange = () => {
-      setIsLandscape(window.innerHeight < window.innerWidth && window.innerHeight < 500)
-    }
-    
-    landscapeMql.addEventListener("change", onChange)
-    onChange()
-    
-    return () => landscapeMql.removeEventListener("change", onChange)
-  }, [])
-
-  return isLandscape
 }
